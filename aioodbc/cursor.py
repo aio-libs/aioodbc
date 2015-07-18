1957c3f6d0c05974306d845ddf0423f8e8fd3cfb
Nickolai Novik <is.infinity@yahoo.com>
2015-07-18 21:40:12 +0300

    implement most pyodbc cursor methods

--- aioodbc/cursor.py
+++ aioodbc/cursor.py
@@ -6,80 +6,136 @@ class Cursor:
         self._conn = connection
         self._impl = impl
         self._loop = connection.loop
-        self._executor = connection._executor
-
-    def _execute(self, func, *args, **kwargs):
-        f = self._conn._execute(func, *args, **kwargs)
-        return f
-
-    @property
-    def description(self):
-        return self._impl.description
 
     def close(self):
-        """Close the cursor now."""
-        self._impl.close()
-
-    @property
-    def closed(self):
-        """Read-only boolean attribute: specifies if the cursor is closed."""
-        return self._impl.closed
+        fut = self._conn._execute(self._impl.close)
+        return fut
 
     @property
     def connection(self):
-        """Read-only attribute returning a reference to the `Connection`."""
         return self._conn
-
-    def execute(self, sql, *params):
-        fut = self._execute(self._impl.execute, sql, *params)
-        return fut
-
-    @asyncio.coroutine
-    def executemany(self, operation, seq_of_parameters):
-        pass
-
-    @asyncio.coroutine
-    def callproc(self, procname, parameters=None, *, timeout=None):
-        pass
-
-    @asyncio.coroutine
-    def fetchone(self):
-        ret = self._impl.fetchone()
-        return ret
-
-    @asyncio.coroutine
-    def fetchmany(self, size=None):
-        if size is None:
-            size = self._impl.arraysize
-        ret = self._impl.fetchmany(size)
-        return ret
-
-    @asyncio.coroutine
-    def fetchall(self):
-        ret = self._execute(self._impl.fetchall)
-        return ret
-
-    @asyncio.coroutine
-    def scroll(self, value, mode="relative"):
-        ret = self._impl.scroll(value, mode)
-        return ret
 
     @property
     def rowcount(self):
-        return self._impl.rowcount
+        return self._conn
 
     @property
-    def rownumber(self):
-        return self._impl.rownumber
+    def description(self):
+        return self._conn.description
 
     @property
-    def lastrowid(self):
-        return self._impl.lastrowid
+    def closed(self):
+        return self._impl.closed
 
     @property
-    def query(self):
-        return self._impl.query
+    def arraysize(self):
+        return self._conn.arraysize
+
+    @arraysize.setter
+    def arraysize(self, size):
+        self._conn.arraysize = size
+
+    def execute(self, sql, *params):
+        fut = self._conn._execute(self._impl.execute, sql, *params)
+        return fut
+
+    def executemany(self, sql, *params):
+        fut = self._conn._execute(self._impl.executemany, sql, *params)
+        return fut
 
     @asyncio.coroutine
+    def setinputsizes(self, *args, **kwargs):
+        return None
+
+    @asyncio.coroutine
+    def setoutputsize(self, *args, **kwargs):
+        return None
+
+    def fetchone(self):
+        fut = self._conn._execute(self._impl.fetchone)
+        return fut
+
+    def fetchall(self):
+        fut = self._conn._execute(self._impl.fetchall)
+        return fut
+
+    def fetchmany(self, size):
+        fut = self._conn._execute(self._impl.fetchmany, size)
+        return fut
+
     def nextset(self):
-        self._impl.nextset()
+        fut = self._conn._execute(self._impl.nextset)
+        return fut
+
+    def tables(self, table=None, catalog=None, schema=None, tableType=None):
+        fut = self._conn._execute(self._impl.tables, table=table,
+                                  catalog=catalog, schema=schema,
+                                  tableType=tableType)
+        return fut
+
+    def columns(self, table=None, catalog=None, schema=None, column=None):
+        fut = self._conn._execute(self._impl.columns, table=table,
+                                  catalog=catalog, schema=schema,
+                                  column=column)
+        return fut
+
+    @asyncio.coroutine
+    def statistics(self, catalog=None, schema=None, unique=False, quick=True):
+        fut = self._conn._execute(self._impl.statistics, catalog=catalog,
+                                  schema=schema, unique=unique, quick=quick)
+        return fut
+
+    def rowIdColumns(self, table, catalog=None, schema=None, nullable=True):
+        fut = self._conn._execute(self._impl.rowIdColumns, table,
+                                  catalog=catalog, schema=schema,
+                                  nullable=nullable)
+        return fut
+
+    def rowVerColumns(self, table, catalog=None, schema=None, nullable=True):
+        fut = self._conn._execute(self._impl.rowVerColumns, table,
+                                  catalog=catalog, schema=schema,
+                                  nullable=nullable)
+        return fut
+
+    def primaryKeys(self, table, catalog=None, schema=None):
+        fut = self._conn._execute(self._impl.primaryKeys, table,
+                                  catalog=catalog, schema=schema)
+        return fut
+
+    # TODO: check source code of pyodbc regadring table argument
+    def foreignKeys(self, table=None, catalog=None, schema=None,
+                    foreignTable=None, foreignCatalog=None,
+                    foreignSchema=None):
+        fut = self._conn._execute(self._impl.foreignKeys, table=table,
+                                  catalog=catalog, schema=schema,
+                                  foreignTable=foreignTable,
+                                  foreignCatalog=foreignCatalog,
+                                  foreignSchema=foreignSchema)
+        return fut
+
+    def getTypeInfo(self, sqlType=None):
+        fut = self._conn._execute(self._impl.getTypeInfo, sqlType)
+        return fut
+
+    def procedures(self, procedure=None, catalog=None, schema=None):
+        fut = self._conn._execute(self._impl.procedures, procedure=procedure,
+                                  catalog=catalog, schema=schema)
+        return fut
+
+    def procedureColumns(self, procedure=None, catalog=None, schema=None):
+        fut = self._conn._execute(self._impl.procedureColumns,
+                                  procedure=procedure, catalog=catalog,
+                                  schema=schema)
+        return fut
+
+    def skip(self, count):
+        fut = self._conn._execute(self._impl.skip, count)
+        return fut
+
+    def commit(self):
+        fut = self._conn._execute(self._impl.commit)
+        return fut
+
+    def rollback(self):
+        fut = self._conn._execute(self._impl.rollback)
+        return fut

8219991ba5f647837b8110545b947dd48dbe398f
Nickolai Novik <nickolai@datarobot.com>
2015-08-07 00:03:37 -0400

    array size and rowcount tests

--- tests/test_cursor.py
+++ tests/test_cursor.py
@@ -1,9 +1,24 @@
+import asyncio
 from tests import base
 
 from tests._testutils import run_until_complete
 
 
 class TestCursor(base.ODBCTestCase):
+
+    @asyncio.coroutine
+    def _prepare_table(self):
+        conn = yield from self.connect()
+        cur = yield from conn.cursor()
+
+        yield from cur.execute("DROP TABLE t1;")
+        yield from cur.execute("CREATE TABLE t1(n INT, v VARCHAR(10));")
+        yield from cur.execute("INSERT INTO t1 VALUES (1, '123.45');")
+        yield from cur.execute("INSERT INTO t1 VALUES (2, 'foo');")
+
+        yield from conn.commit()
+        yield from cur.close()
+        yield from conn.close()
 
     @run_until_complete
     def test_cursor(self):
@@ -52,4 +67,27 @@ class TestCursor(base.ODBCTestCase):
         expected = (('n', int, None, 10, 10, 0, True),
                     ('v', str, None, 10, 10, 0, True))
         self.assertEqual(cur.description, expected)
+        yield from conn.close()
+
+    @run_until_complete
+    def test_rowcount_with_table(self):
+        yield from self._prepare_table()
+        conn = yield from self.connect()
+        cursor = yield from conn.cursor()
+        yield from cursor.execute("SELECT * FROM t1;")
+        yield from cursor.fetchall()
+        # sqlite does not provide working rowcount attribute
+        # http://stackoverflow.com/questions/4911404/in-pythons-sqlite3-
+        # module-why-cant-cursor-rowcount-tell-me-the-number-of-ro
+        self.assertEqual(cursor.rowcount, 0)
+        yield from conn.close()
+
+    @run_until_complete
+    def test_arraysize(self):
+        conn = yield from self.connect()
+        cursor = yield from conn.cursor()
+        self.assertEqual(1, cursor.arraysize)
+
+        cursor.arraysize = 10
+        self.assertEqual(10, cursor.arraysize)
         yield from conn.close()

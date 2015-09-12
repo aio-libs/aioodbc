0eeaffcd10f285ee46faf1730d3306bf24d43793
Nickolai Novik <is.infinity@yahoo.com>
2015-09-13 00:59:47 +0300

    connection pool implementation

--- aioodbc/__init__.py
+++ aioodbc/__init__.py
@@ -1,7 +1,7 @@
 from .connection import connect, Connection
-
+from .pool import create_pool, Pool
 
 __version__ = '0.0.1'
 __all__ = ['connect', 'Connection']
 
-(connect, Connection)
+(connect, Connection, create_pool, Pool)

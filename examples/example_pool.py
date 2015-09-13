f055c3525ddfc310ff0ea0c0a6a542bf8a71ccc2
Nickolai Novik <is.infinity@yahoo.com>
2015-09-14 00:03:43 +0300

    add pool example

---
+++ examples/example_pool.py
@@ -0,0 +1,22 @@
+import asyncio
+import aioodbc
+
+
+loop = asyncio.get_event_loop()
+
+
+async def test_pool():
+    dsn = 'Driver=SQLite;Database=sqlite.db'
+    pool = await aioodbc.create_pool(dsn=dsn, loop=loop)
+
+    async with (await pool) as conn:
+        cur = await conn.cursor()
+        await cur.execute("SELECT 42;")
+        r = await cur.fetchall()
+        print(r)
+        await cur.close()
+        await conn.ensure_closed()
+    pool.close()
+    await pool.wait_closed()
+
+loop.run_until_complete(test_example())

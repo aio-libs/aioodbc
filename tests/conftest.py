88df83b3a78b19dc6c5a217ec3e27e00ee724989
Nickolai Novik <is.infinity@yahoo.com>
2015-10-08 23:14:28 +0300

    fix fixture for loop

--- tests/conftest.py
+++ tests/conftest.py
@@ -117,7 +117,12 @@ def pytest_pyfunc_call(pyfuncitem):
         loop = funcargs['loop']
         testargs = {arg: funcargs[arg]
                     for arg in pyfuncitem._fixtureinfo.argnames}
-        loop.run_until_complete(pyfuncitem.obj(**testargs))
+
+        if not asyncio.iscoroutinefunction(pyfuncitem.obj):
+            func = asyncio.coroutine(pyfuncitem.obj)
+        else:
+            func = pyfuncitem.obj
+        loop.run_until_complete(func(**testargs))
         return True
 
 

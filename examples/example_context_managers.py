518c6e067a26fb7664204547bc0817b78b0b7fb8
Nickolai Novik <is.infinity@yahoo.com>
2015-10-17 00:54:34 +0300

    add context manager example

---
+++ examples/example_context_managers.py
@@ -0,0 +1,18 @@
+import asyncio
+import aioodbc
+
+
+loop = asyncio.get_event_loop()
+
+
+async def test_example():
+    dsn = 'Driver=SQLite;Database=sqlite.db'
+
+    async with aioodbc.create_pool(dsn=dsn, loop=loop) as pool:
+        async with pool.get() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute('SELECT 42;')
+                val = await cur.fetchone()
+                print(val)
+
+loop.run_until_complete(test_example())

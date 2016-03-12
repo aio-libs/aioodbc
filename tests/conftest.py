4095d5823dfa76a588df438a381a4b4ea32abc8c
Nickolai Novik <is.infinity@yahoo.com>
2016-03-12 21:24:43 +0200

    unused port fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,19 @@
 import asyncio
 import os
+import socket
 from concurrent.futures import ThreadPoolExecutor
 
 import pytest
 import aioodbc
+
+
+@pytest.fixture(scope='session')
+def unused_port():
+    def f():
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('127.0.0.1', 0))
+            return s.getsockname()[1]
+    return f
 
 
 @pytest.fixture
